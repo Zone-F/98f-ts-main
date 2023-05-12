e673ed8877e633698e9b4ffcd009a0962755323a
zhangyongfa <zhangyongfa@xindecolink.com>
2023-05-12 17:34:08 +0800

    5.11

--- type-challenges/yd/5.11.js
+++ type-challenges/yd/5.11.js
@@ -1,32 +1,32 @@
 // 使用JavaScript/TypeScript实现树的先中后序遍历（不能使用递归）
 const data = {
-  name: "A",
-  children: [
-    {
-      name: "B1",
-      children: [
-        {
-          name: "C1",
-          children: [
-            {
-              name: "D",
-              children: [
-                {
-                  name: "D1",
-                  children: [{ name: "F1 " }, { name: "F2" }],
-                },
-                { name: "D2" },
-              ],
-            },
-          ],
-        },
-      ],
+  val: 50,
+  left: {
+    val: 10,
+    left: {
+      val: 5,
+      left: null,
+      right: null,
     },
-    {
-      name: "B2",
-      children: [{ name: "C2" }, { name: "C3" }],
+    right: {
+      val: 15,
+      left: null,
+      right: null,
     },
-  ],
+  },
+  right: {
+    val: 70,
+    left: {
+      val: 60,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 80,
+      left: null,
+      right: null,
+    },
+  },
 };
 
 // 先序遍历 - 右子树先进栈
@@ -36,18 +36,46 @@ function preorderTraverse(root) {
 
   while (stack.length) {
     let cur = stack.pop();
-    result.push(cur.name);
-    cur.children && stack.push(cur.children);
+    result.push(cur.val);
+    if (cur.right) stack.push(cur.right);
+    if (cur.left) stack.push(cur.left);
   }
-  return result
+  return result;
 }
 console.log('先序遍历', preorderTraverse(data));
 
 // 中序遍历
-function inorderTraverse() {
+function inorderTraverse(root) {
+  let stack = [root];
+  let result = [];
+  let node = root;
 
+  while (stack.length || node !== null) {
+    if (node) {
+      stack.push(node);
+      node = node.left
+    } else {
+      node = stack.pop();
+      result.push(node.val);
+      node = node.right;
+    }
+  }
+  return result;
 }
+console.log('中序遍历', inorderTraverse(data));
+
 // 后序遍历
-function postorderTraversal() {
+// 
+function postorderTraversal(root) {
+  let stack = [root];
+  let result = [];
 
+  while (stack.length) {
+    let cur = stack.pop();
+    result.unshift(cur.val);
+    if (cur.right) stack.push(cur.right);
+    if (cur.left) stack.push(cur.left);
+  }
+  return result;
 }
+console.log('后序遍历', postorderTraversal(data));
